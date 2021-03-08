ceffd20b60076fc7a08f1961753814a767b191df
Technokarts@123 <priyanka.nevase@technokarts.com>
2021-03-08 19:49:28 +0530

    Commit Pritesh

---
+++ app/src/main/java/centriotech/hrinterview/Home.java
@@ -0,0 +1,14 @@
+package centriotech.hrinterview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
